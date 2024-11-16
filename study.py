0b0708cc79dcddd7d1eebfcc92080a15ce795457
Soomin-02 <dlgndud321@naver.com>
2024-11-16 16:39:39 +0900

    while 반복문을 이용한 Python Casino 예제

--- study.py
+++ study.py
@@ -1,9 +1,20 @@
-user_choice = int(input("Choice number."))
-pc_choice = 20
+from random import randint
+print("Welcome to Python Casino!")
+Player = int(input("Choice number"))
 
-if user_choice == pc_choice:
-    print("You won!")
-elif user_choice > pc_choice:
-    print("Lower!")
-elif user_choice < pc_choice:
-    print("Higher!")
+Banker = randint(1,10)
+playing = True
+    
+while playing:
+    if Player == Banker:
+        print("Banker is", Banker, "!")
+        print("Tie!")
+        playing = False
+    elif Player > Banker:
+        print("Banker is", Banker, "!")
+        print("You won!")
+        playing = False
+    elif Player < Banker:
+        print("Banker is", Banker, "!")
+        print("You lose!")
+        playing = False
